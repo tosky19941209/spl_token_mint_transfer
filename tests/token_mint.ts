6cd341762d0ab381c1c4be1359d635a625db34e8
tosky19941209 <felixeliass1994@gmail.com>
2024-09-06 16:04:02 +0900

    feat: token mint is successed

--- tests/token_mint.ts
+++ tests/token_mint.ts
@@ -1,6 +1,8 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { TokenMint } from "../target/types/token_mint";
+import { TOKEN_PROGRAM_ID } from "@coral-xyz/anchor/dist/cjs/utils/token";
+import { createAccount } from "@solana/spl-token";
 
 describe("token_mint", () => {
   // Configure the client to use the local cluster.
@@ -9,9 +11,38 @@ describe("token_mint", () => {
 
   const program = anchor.workspace.TokenMint as Program<TokenMint>;
 
+  const mintToken = anchor.web3.Keypair.generate();
+  const associateTokenProgram = new anchor.web3.PublicKey("ATokenGPvbdGVxr1b2hvZbsiqW5xWH25efTNsLJA8knL");
+  const tokenAccount = anchor.utils.token.associatedAddress({ mint: mintToken.publicKey, owner: provider.publicKey });
+  const ta = anchor.web3.PublicKey.findProgramAddressSync(
+    [provider.publicKey.toBuffer(), TOKEN_PROGRAM_ID.toBuffer(), mintToken.publicKey.toBuffer()],
+    associateTokenProgram
+  )[0];
+
+  let tokenAccountKeyPair = anchor.web3.Keypair.generate()
+
+
   it("Is initialized!", async () => {
     // Add your test here.
     const tx = await program.methods.initialize().rpc();
     console.log("Your transaction signature", tx);
   });
+
+  it("Token mint", async () => {
+    console.log(mintToken.publicKey.toBase58())
+    console.log(tokenAccount.toBase58())
+    try {
+      const tx = await program.methods.createToken(9, new anchor.BN(10 ** 9 * 100))
+        .accounts({
+          mintToken: mintToken.publicKey,
+          tokenAccount: tokenAccount,
+          associateTokenProgram,
+        })
+        .signers([mintToken])
+        .rpc();
+      console.log("Your transaction signature", tx);
+    } catch (error) {
+      console.log(error)
+    }
+  })
 });
